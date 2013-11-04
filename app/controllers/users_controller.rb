a9528b349f8960c56d2ee4a62df03a1c11a47343
Brian Weiss <brianmweiss@gmail.com>
2013-11-04 12:11:05 -0500

    Removed update action from users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_filter :authenticate_user!, :only => [ :index, :update, :destroy ]
+  before_filter :authenticate_user!, :only => [ :index, :destroy ]
 
   def index
     authorize! :index, User, :message => 'Not authorized to perform this action.'
@@ -9,16 +9,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     authorize! :show, @user, :message => 'Not authorized to perform this action.'
-  end
-  
-  def update
-    @user = User.find(params[:id])
-    authorize! :update, @user, :message => 'Not authorized to perform this action.'
-    if @user.update_attributes(params[:user], :as => :admin)
-      redirect_to users_path, :notice => "User updated."
-    else
-      redirect_to users_path, :alert => "Unable to update user."
-    end
   end
     
   def destroy
