1894b75ed3c748c8fbe449cd66282b27065be8e4
Brian Weiss <brianmweiss@gmail.com>
2013-11-22 11:07:37 -0500

    Upgrade to devise 3.2.1

--- app/models/user.rb
+++ app/models/user.rb
@@ -43,7 +43,7 @@ class User < ActiveRecord::Base
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable, :recoverable, :rememberable,
-         :trackable, :validatable, :token_authenticatable, :lockable
+         :trackable, :validatable, :lockable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :role_ids, :as => :admin
