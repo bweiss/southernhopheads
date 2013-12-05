a0ca9b655f191e7c76b7d157d804070ab44b4a3c
Brian Weiss <brianmweiss@gmail.com>
2013-12-05 17:24:04 -0500

    Split articles and events part 2
    Added rake task to move articles with an event flag to the new Events
    resource.
    
    Also added a private home controller method to retrieve featured
    articles and events sorted together as one resource.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,7 @@ class HomeController < ApplicationController
 
   def index
     @articles = Article.published.order("updated_at DESC").limit(6)
+    @articles_and_events = find_featured_articles_and_events(:order => "updated_at DESC", :limit => 6)
   end
 
   def about
@@ -17,4 +18,31 @@ class HomeController < ApplicationController
     @new_comments = Comment.order('created_at DESC').limit(4)
     @unconfirmed_users = User.where(:confirmed_at => [nil, false])
   end
+
+  private
+
+    #
+    # Fetch the most recent articles and events, sorted and limited together as one resource.
+    # This returns an array of hashes with each array element representing one row of returned data.
+    #
+    def find_featured_articles_and_events(options = { :order => nil, :limit => nil })
+      results = Array.new
+      cols = [:id, :title, :content, :user_id, :start_at, :end_at, :location, :allow_comments, :featured, :published, :created_at, :updated_at]
+
+      query  = "(SELECT #{cols.join(', ')}, 'article' AS type"
+      query << " FROM articles WHERE published = 1 AND featured = 1)"
+      query << " UNION"
+      query << " (SELECT #{cols.join(', ')}, 'event' AS type"
+      query << " FROM events WHERE published = 1 AND featured = 1)"
+      query << " ORDER BY #{order}" unless options[:order].nil?
+      query << " LIMIT #{limit}" unless options[:limit].nil?
+
+      rows = ActiveRecord::Base.connection.execute(query)
+
+      rows.each do |row|
+        results.push(Hash[*cols.zip(row).flatten])
+      end
+
+      return results
+    end
 end
